ed9d4ca907977020984b2092a1486bd251bfc0dc
Hakan Emin Ünal <20010310044@ogrenci.bartin.edu.tr>
2023-03-14 14:33:04 +0300

    kamp2_odev2

---
+++ classExercise/classes.py
@@ -0,0 +1,10 @@
+
+class Banka:
+    def krediBasvur(self): #self = this
+        print("Kredi basvurusu yapildi")
+    
+    def krediHesapla(self):
+        print("hesaplamalar yapildi")
+
+banka = Banka()
+banka.krediBasvur()
---
+++ classExercise/person.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, lastName):
+        self.name = name
+        self.lastName = lastName
+
+
+musteri1 = Person("hakan","unal")
+musteri2 = Person("esra","deniz")
+
+print(musteri1.name)
